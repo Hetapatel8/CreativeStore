3586a7710fa978ab0b504dd463cbf1ea5b889b84
Heta Patel <patelheta882@gmail.com>
2024-11-25 22:57:18 +0530

    factor: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./layout/*.liquid",
+    "./templates/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./assets/*.js",
+    "./css/*.css",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
